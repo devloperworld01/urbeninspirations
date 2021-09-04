910d85a7bea413ea5b924d282ab9a03946a642a0
devloperworld01 <90109671+devloperworld01@users.noreply.github.com>
2021-09-05 00:17:42 +0530

    Update server.py

--- server.py
+++ server.py
@@ -3,9 +3,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/.well-known/acme-challenge/<copy last part of URL from certbot console>")
 def hello():
-    return "I love this site!!!"
+    return "<copy data from certbot console>"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
