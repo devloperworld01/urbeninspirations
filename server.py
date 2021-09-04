2e600c4bdfe6cd2810b880f1c34104c28b0586a1
devloperworld01 <90109671+devloperworld01@users.noreply.github.com>
2021-09-05 00:13:41 +0530

    Update server.py

--- server.py
+++ server.py
@@ -3,13 +3,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/.well-known/acme-challenge/kcMNDL1qCzRvuFpmqwfyM3Rc1iRmxf1MFwzD5Je9s7o.5249X9NOnpSqQPKt8MkLdEtedSVDgAyyxwv9BRg1bGg")
-# e.g. /.well-known/acme-challenge/lzrajCaq8vbw5Qz2o_XXXXXXXXXXXXXXXXX
+@app.route("/")
 def hello():
-    return "kcMNDL1qCzRvuFpmqwfyM3Rc1iRmxf1MFwzD5Je9s7o.5249X9NOnpSqQPKt8MkLdEtedSVDgAyyxwv9BRg1bGg"
-    # e.g. return "lzrajCaq8vbw5Qz2o_XXXXXXXXXXXXXXXXXXz4RnfWtLKaIc6EdhsOsr4fb6RFZuUoabZW5dPW36cmc"
+    return "I love this site!!!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
-
